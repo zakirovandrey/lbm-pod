7e4ffaf27deb5e2bf75acac836fddd220c14b159
Andrey Zakirov <zakirov@kintechlab.com>
2020-07-07 21:05:13 +0300

    fix D2Q5 USUAL scheme

--- D2Q5_USUAL_consts.cuh
+++ D2Q5_USUAL_consts.cuh
@@ -8,17 +8,36 @@ constexpr const int3 _e[QN] = {
   /* 3 */ {-1, 0, 0}, 
   /* 4 */ { 0,-1, 0}, 
 };
-const ftype W0=0.5;
-const ftype W1=0.125;
-const ftype W2=0;
-const ftype W3=0;
 
-const ftype cs2 = dx*dx/3.;
+constexpr const ftype cs2 = 1./3.;
+const ftype TLat=cs2;
+constexpr ftype W0get(const ftype Tv) { return 1./2.; }
+constexpr ftype W1get(const ftype Tv) { return 1./8.; }
+constexpr ftype W2get(const ftype Tv) { return 0; }
+
+const ftype Tmin = 0;
+const ftype Tmax = 100;
 
 
 //#define NON_ISOTHERMAL_RELAXATION
 
 #define WEIGHTS_MANUALLY
+
+#define W0 W0get(Tc)
+#define W1 W1get(Tc)
+#define W2 W2get(Tc)
+#define W3 W3get(Tc)
+#define W4 W4get(Tc)
+#define W5 W5get(Tc)
+
+constexpr ftype w_get(const int i, const ftype Tc) {
+  const ftype arr[QN] = {
+  W0, W1,W1,W1,W1
+  };
+  return arr[i];
+}
+#define TO_SEQ_ELEM_WGET(z, n, text) w_get(n,TLat),
 constexpr ftype _w[QN] = {
-  W0, W1, W1, W1, W1
+  BOOST_PP_REPEAT( QN, TO_SEQ_ELEM_WGET, w_get )
 };
+
