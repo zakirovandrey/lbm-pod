becdedc5982c9d1a56af3143875b90b582db9b25
Andrey Zakirov <zakirov@kintechlab.com>
2020-07-10 23:50:31 +0300

    fix D1Q3

--- D1Q3_USUAL_consts.cuh
+++ D1Q3_USUAL_consts.cuh
@@ -1,20 +1,43 @@
 #define DIM 1
 #define QN 3
 #define QN_IN_DIM 6   // QN*2^DIM
-constexpr const int3 _e[QN] = { { 0, 0, 0}, { 1, 0, 0}, { -1, 0, 0} };
-constexpr const ftype _cs2 = dx*dx/3.;
-const ftype cs2 = _cs2;
+
+const int ec1=1;
+const int ec1d=ec1*ec1;
+
+constexpr const ftype _cs2 = 1./3.;
+constexpr const ftype cs2 = _cs2;
 const ftype TLat=cs2;
 
-const ftype W0=2./3;
-const ftype W1=1./6;
-const ftype W2=0;
-const ftype W3=0;
+constexpr ftype W0get(const ftype Tv) { return 2./3.; }
+constexpr ftype W1get(const ftype Tv) { return 1./6.; }
+constexpr ftype W2get(const ftype Tv) { return 0; }
+
+const ftype Tmin = 0;
+const ftype Tmax = 100.0;
 
 
 //#define NON_ISOTHERMAL_RELAXATION
 
-#define WEIGHTS_MANUALLY
-constexpr ftype _w[QN] = {
-  W0, W1, W1
+constexpr const int3 _e[QN] = {
+  {0   , 0   , 0 }, {+ec1, 0   , 0 }, {-ec1, 0   , 0 }
 };
+
+#define WEIGHTS_MANUALLY
+
+#define W0 W0get(Tc)
+#define W1 W1get(Tc)
+#define W2 W2get(Tc)
+#define W3 W3get(Tc)
+#define W4 W4get(Tc)
+#define W5 W5get(Tc)
+
+constexpr ftype w_get(const int i, const ftype Tc) {
+  const ftype arr[QN] = {W0, W1, W2};
+  return arr[i];
+}
+#define TO_SEQ_ELEM_WGET(z, n, text) w_get(n,TLat),
+constexpr ftype _w[QN] = {
+  BOOST_PP_REPEAT( QN, TO_SEQ_ELEM_WGET, w_get )
+};
+
